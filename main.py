85185ac39f9bf69caa30c47c424dfc59cdf7a881
Emanuel Henkel Gonçalves <emanuel.henkel@gmail.com>
2024-10-13 11:10:11 -0300

    Arquivo main
    Arquivo raiz, responsável pela execução de funções relacionadas ao DB e API

---
+++ main.py
@@ -0,0 +1,19 @@
+
+#Biblioteca os para comandos no cmd e biblioteca json pra converter dict para json e vice-versa
+import os
+import json
+
+#Verificar se arquivo com parâmetros do usuário existe, se não, ele é criado
+if 'params.json' not in os.listdir():
+    db_user_data = {
+        'user': str(input('Insira o username: ')),
+        'password': str(input('Insira a senha: '))
+    }
+
+    with open('params.json', 'w') as params_file:
+        json.dump(db_user_data, params_file)
+
+from db_utils.create_database import create_database
+
+create_database()
+
